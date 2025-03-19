ea297162c45fd2e2708b6e637ff703b4a498d4b0
Passion <oscarpassion36@gmail.com>
2025-03-19 22:45:03 +0800

    refactor: change tag button to radio

--- src/App.tsx
+++ src/App.tsx
@@ -12,29 +12,29 @@ function App() {
     return (
         <div className="m-12 flex flex-col h-full">
             <div className="flex justify-between items-center">
-                <Title title={ testTitle.title } tags={ testTitle.tags }/>
-                <ThemeSwitcher/>
+                <Title title={ testTitle.title } tags={ testTitle.tags } />
+                <ThemeSwitcher />
             </div>
 
             <div className="divider my-[4px]" />
 
             <div className="flex flex-row">
                 <div className="space-y-5 place-items-center basis-2/5">
-                    <Images images={ testImages }/>
-                    <NoteContent content={ testContent }/>
-                    <div className="w-1/2">
-                        <TagButton/>
+                    <Images images={ testImages } />
+                    <NoteContent content={ testContent } />
+                    <div className="w-3/5 h-10">
+                        <TagButton />
                     </div>
                     <div className="self-center">
-                        <text>1 of 10</text>
+                        <p>1 of 10</p>
                     </div>
                 </div>
 
-                <div className="divider divider-horizontal"/>
+                <div className="divider divider-horizontal" />
 
                 <div className="basis-3/5 justify-items-end">
-                    <CommentList comments={ testComment }/>
-                    <NoteSwitcher/>
+                    <CommentList comments={ testComment } />
+                    <NoteSwitcher />
                 </div>
             </div>
         </div>
--- src/components/TagButton.tsx
+++ src/components/TagButton.tsx
@@ -2,17 +2,32 @@ import { FaRegQuestionCircle, FaRegThumbsDown, FaRegThumbsUp } from "react-icons
 
 const TagButton = () => {
     return (
-        <div className="flex gap-2">
-            <div className="btn btn-primary flex-auto">
-                <FaRegThumbsDown />
-            </div>
-            <div className="btn btn-primary flex-auto">
-                <FaRegQuestionCircle />
-            </div>
-            <div className="btn btn-primary flex-auto">
-                <FaRegThumbsUp />
-            </div>
+        <div className="flex items-center space-x-2 h-full">
+            <label className="flex items-center cursor-pointer flex-auto h-full">
+                <input type="radio" name="option" className="hidden peer" />
+                <div className="p-2 rounded-lg flex-auto h-full
+                content-center justify-items-center bg-gray-500 peer-checked:bg-red-400">
+                    <FaRegThumbsDown />
+                </div>
+            </label>
+
+            <label className="flex items-center cursor-pointer flex-auto h-full">
+                <input type="radio" name="option" className="hidden peer" />
+                <div className="p-2 rounded-lg flex-auto h-full
+                content-center justify-items-center bg-gray-500 peer-checked:bg-blue-400">
+                    <FaRegQuestionCircle />
+                </div>
+            </label>
+
+            <label className="flex items-center cursor-pointer flex-auto h-full">
+                <input type="radio" name="option" className="hidden peer" />
+                <div className="p-2 rounded-lg flex-auto h-full
+                    content-center justify-items-center bg-gray-500 peer-checked:bg-green-400">
+                    <FaRegThumbsUp />
+                </div>
+            </label>
         </div>
+
     );
 };
 
